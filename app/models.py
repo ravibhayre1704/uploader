e7f7da5a77160f72e8bb8c4967ec4cc92a64c327
ravishanka407 <113426393+ravishanka407@users.noreply.github.com>
2022-10-04 14:58:05 +0530

    add image

---
+++ app/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+class Image(models.Model):
+    picture = models.ImageField(upload_to="myimage")
+    date = models.DateTimeField(auto_now_add=True)
